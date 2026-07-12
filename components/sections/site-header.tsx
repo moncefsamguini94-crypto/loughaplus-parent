76586afc883b860d37b8ef80efbde3b1c946dc0f
Lougha Plus <videoediting55ads@gmail.com>
2026-07-12 13:53:56 +0000

    Replace header with navy LOUGHA PLUS banner as-is

--- components/sections/site-header.tsx
+++ components/sections/site-header.tsx
@@ -1,6 +1,6 @@
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-40 w-full bg-black">
+    <header className="sticky top-0 z-40 w-full bg-[#001A57]">
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         src="/lougha-plus-header-banner.png"
